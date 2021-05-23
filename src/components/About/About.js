d64d5aed99c73a86e5a4c150f10b8c5d54b11b1a
DianaGornic <gornicdiana.199@gmail.com>
2021-05-23 17:36:03 +0300

    created navigation bar, home section and about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,22 @@
+import classes from './About.module.css';
+import PageHeader from './../PageHeader/PageHeader';
+import me from './../../img/photoCV.jpeg';
+
 const About = () => {
 
     return (
-        <div>
-            <h1>About </h1>
+        <div className={ classes.AboutMe}>
+            <PageHeader title={'About Me'} />
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h1> Hello! I'm Diana.</h1>
+                    <h2>Third year student</h2>
+                    <p>A tech lover with proven programming skills and a logical mind, ready to start a career!</p>
+                </div>
+                <div className={classes.Photo}>
+                    <img className={classes.Me} src={me} alt='me'></img>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,31 @@
+import classes from "./NavBar.module.css";
+
 const NavBar = () => {
 
     return (
-        <div>
-            <h1>Navigation Bar </h1>
+        <div className={classes.NavBar}>
+            <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/#about">About</a>
+                </li>
+                <li>
+                    <a href="/#education">Education</a>
+                </li>
+                <li>
+                    <a href="/#projects">Projects</a>
+                </li>
+                <li>
+                    <a href="/#skills">Skills</a>
+                </li>
+                <li>
+                    <a href="/#contact">Contact</a>
+                </li>
+                </ul>
+            </nav>
         </div>
         );
 }
